08b9910e790c803c2af27bf44fce00772c07a38f
John Rogers <jfrogers37@gmail.com>
2026-07-01 13:37:32 -0500

    adding firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.detekt) apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
